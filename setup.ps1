eda94e4b62cdf8cd9c84c893da22e896d6c5073a
Rasmus Frederiksen <rasmus.frederiksen@storefront.be>
2020-04-21 03:29:23 -0700

    Update setup.ps1
    Added scoop instructions, still needs fieldtesting

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,12 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
 # Use the verbose version for readability
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+# Scoop NEEDS git to operate at all, so do this first
+scoop install git
+
+# Add the extras bucket to install all of the personal apps, could add more here, newline for each thou
+scoop bucket add extras
+
+# Personal apps
+scoop install firefox thunderbird 1password-cli discord teamspeak3 vlc
